5693f6a984f626b5954ee142f5331918face42f9
aragalucas <126295197+aragalucas@users.noreply.github.com>
2024-09-23 13:26:26 +0000

    atualizando pois não esta funcionando

--- nome_e_idade.py
+++ nome_e_idade.py
@@ -1,9 +1,10 @@
 import sys
-nome = input("Nome: ")
-idade = input("Idade: ")
-main()
-while True:
-    try:
-        if len(nome) <= 0:
-        main();
-            
+def main():
+    nome = input("Nome: ")
+    idade = int(input("Idade: "))
+
+def main():
+    if len(nome) <= 0:
+        main()
+    if idade < 0:
+        main()
