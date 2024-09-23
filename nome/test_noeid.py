fcb55dac05d13cf820fa8f5796fe710be43c7694
aragalucas <126295197+aragalucas@users.noreply.github.com>
2024-09-23 22:28:50 +0000

    vesão fina

--- nome/test_noeid.py
+++ nome/test_noeid.py
@@ -1,3 +1,7 @@
 from nome_e_idade import idds
 def test_negativo():
-    assert idds(0) == ""
+    assert idds(-57) == "===numero negativo==="
+    assert idds(-1) == "===numero negativo==="
+def test_positivo():
+    assert idds(0) == "" 
+    assert idds(20) == ""
