b628533e23627893f7e4edb7b2870d9c0c47a335
aragalucas <126295197+aragalucas@users.noreply.github.com>
2024-11-21 19:47:01 +0000

    redescobrindo outras formas

--- nome/test_noeid.py
+++
@@ -1,7 +0,0 @@
-from nome_e_idade import idds
-def test_negativo():
-    assert idds(-57) == "===numero negativo==="
-    assert idds(-1) == "===numero negativo==="
-def test_positivo():
-    assert idds(0) == "" 
-    assert idds(20) == ""
